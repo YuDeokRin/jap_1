07632035ac51e9b20e28fa9e7db8b4457065e7c8
YuDeokRin <ejrfls12@naver.com>
2022-01-28 23:38:24 +0900

    에러 못찾음

--- src/test/java/jpabook/japshop/MemberRepositoryTest.java
+++ src/test/java/jpabook/japshop/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ import jpabook.japshop.Member;
 import jpabook.japshop.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
